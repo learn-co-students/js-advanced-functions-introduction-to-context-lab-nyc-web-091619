2601997a3b7a0bdce294ea8a5bddd921c1a79220
Altaf Quadri <altafmquadri@gmail.com>
2019-11-08 22:21:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-// Your code here
+function createEmployeeRecord(employeeInfo) {
+    return {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1], 
+        title: employeeInfo[2], 
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(function(employeeInfo){
+        return createEmployeeRecord(employeeInfo)
+    })
+}
+
+function createTimeInEvent(record, date) {
+    record.timeInEvents.push(
+        {
+            type: "TimeIn",
+            date: date.split(' ')[0],
+            hour: parseInt(date.split(' ')[1].split(':')[0])
+        })
+    return record
+}
+
+function createTimeOutEvent(record, date) {
+    record.timeOutEvents.push(
+        {
+            type: "TimeOut",
+            date: date.split(' ')[0],
+            hour: parseInt(date.split(' ')[1].split(':')[0])
+        })
+    return record
+}
+
+function hoursWorkedOnDate(record, date) {
+    let timeIn = record.timeInEvents.find(function(record){
+        return record.date === date
+    })
+
+    let timeOut = record.timeOutEvents.find(function(record){
+        return record.date === date
+    })
+
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(record, date) {
+    return hoursWorkedOnDate(record, date) * record.payPerHour
+}
+
+function allWagesFor(record) {
+    let allDates = record.timeInEvents.map(function(rtIE) {
+        return rtIE.date
+    })
+
+    let payable = allDates.reduce(function(sum, date){
+        return sum + wagesEarnedOnDate(record, date)
+    }, 0)
+
+    return payable
+}
+
+function findEmployeeByFirstName(records, name) {
+    return records.find(function(record){
+        return record.firstName === name
+    })
+}
+
+function calculatePayroll(records) {
+    return records.reduce(function(sum, record){
+        return sum + allWagesFor(record)
+    }, 0)
+}
